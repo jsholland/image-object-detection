feddea1433d3997acccf3b7e1ee6fdc0065aaf14
jsholland <johnholland77@gmail.com>
2022-08-18 12:17:59 -0500

    remove comment

--- src/main/java/com/johnhollandheb/api/service/ImageService.java
+++ src/main/java/com/johnhollandheb/api/service/ImageService.java
@@ -35,7 +35,6 @@ import java.util.stream.StreamSupport;
 @Component
 @Slf4j
 public class ImageService {
-//    private static final String DEFAULT_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
 
     @Autowired
     ImageRepository imageRepository;
